4188e294f0c4d5e07ee85bdd91f5118696c744dc
kanye <burmester90@gmail.com>
2017-06-03 03:52:26 +0200

    Site updated to 35180ba

--- javascripts/site.js
+++ javascripts/site.js
@@ -1,12 +1,1 @@
-// This is where it all goes :)
-
-
-// moves navbar to bottom when window is less than 640px wide
-
-$(window).on('resize', function() {
-  if ($(window).width() < 640) {
-      $("#expertise").appendTo("#content-bar");
-  } else {
-      $("#expertise").appendTo("#sidebar");;
-  }
-});
+$(window).on("resize",function(){$(window).width()<640?$("#expertise").appendTo("#content-bar"):$("#expertise").appendTo("#sidebar")});
